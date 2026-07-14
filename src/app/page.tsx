e974629d43c02e861653a5962d2da07710193c2d
Kyle Worrall <kyleworrall54@gmail.com>
2026-07-14 13:36:03 -0700

    Reduce Atlas header mark size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
       <header className="site-header">
         <a className="brand-link" href="#top" aria-label="Atlas Landing home">
           <span className="brand-logo-wrap">
-            <Image className="brand-logo" src="/atlas-arrow.svg" alt="" width={40} height={48} priority />
+            <Image className="brand-logo" src="/atlas-arrow.svg" alt="" width={20} height={24} priority />
           </span>
           <span className="wordmark">Atlas Landing</span>
         </a>
